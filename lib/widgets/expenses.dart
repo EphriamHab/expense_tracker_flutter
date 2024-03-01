f44c78f8e91b04df2c0e91adc9cf0eeea7e2b146
EphriamHab <ephremhabtamu2015@gmail.com>
2024-03-02 01:26:15 +0300

    expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker_flutter/widgets/expenses_list/expenses_list.dart
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_flutter/models/expense.dart';
 
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -29,6 +30,19 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      appBar: AppBar(
+        title:Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          const Text("Expense Tracker"),
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon(Icons.add),
+          ),
+        ],
+        ),
+
+      ),
       body: Column(
         children: [
          const Text("The chart"),
